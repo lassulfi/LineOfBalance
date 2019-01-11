646a5619503d7e96ff68d7b70408b02a6563bbe1
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-01-10 16:45:31 -0800

    Created Task entity object

---
+++ LineOfBalance/entities/Task.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LineOfBalance.entities
+{
+    class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Duration { get; set; }
+        public double TotalSlack { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime LateStart { get; set; }
+        public List<Task> predecessors;
+
+        public Task(int id, string name, double duration, double totalSlack, DateTime start, DateTime lateStart)
+        {
+            Id = id;
+            Name = name;
+            Duration = duration;
+            TotalSlack = totalSlack;
+            Start = start;
+            LateStart = lateStart;
+            predecessors = new List<Task>();
+        }
+
+        public void AddPredecessor(Task t)
+        {
+            predecessors.Add(t);
+        }
+
+        public void removePredecessor(Task t)
+        {
+            predecessors.Remove(t);
+        }
+    }
+}
