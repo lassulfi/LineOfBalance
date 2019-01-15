a4acf6527c7b6835fa022b4333a39a662381ef0d
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-01-14 16:09:43 -0800

    Starting elaborating the main form view of the VSTO add-in

---
+++ LineOfBalance/views/CalculationForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace LineOfBalance.views
+{
+    partial class CalculationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbRepetitiveUnits = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbProjectDuration = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of repetitive units:";
+            // 
+            // tbRepetitiveUnits
+            // 
+            this.tbRepetitiveUnits.Location = new System.Drawing.Point(36, 40);
+            this.tbRepetitiveUnits.Name = "tbRepetitiveUnits";
+            this.tbRepetitiveUnits.Size = new System.Drawing.Size(139, 20);
+            this.tbRepetitiveUnits.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Total project duration:";
+            // 
+            // tbProjectDuration
+            // 
+            this.tbProjectDuration.Location = new System.Drawing.Point(36, 79);
+            this.tbProjectDuration.Name = "tbProjectDuration";
+            this.tbProjectDuration.Size = new System.Drawing.Size(139, 20);
+            this.tbProjectDuration.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Name of the repetitive unit (i.e.: house):";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(36, 118);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(189, 20);
+            this.tbName.TabIndex = 5;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(337, 38);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(161, 23);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Calculate Line of Balance";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            // 
+            // CalculationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 311);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbProjectDuration);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbRepetitiveUnits);
+            this.Controls.Add(this.label1);
+            this.Name = "CalculationForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CalculationForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbRepetitiveUnits;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbProjectDuration;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
